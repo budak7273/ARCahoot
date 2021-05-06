9d807b295be8a14cc9422254c2883540e3a56187
Robb <computerguy440+gh@gmail.com>
2021-05-05 23:21:00 -0400

    Get ready to set room key and players list at runtime. Center start button

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -2,15 +2,28 @@
  * @fileoverview
  * Provides the JavaScript interactions for all pages.
  *
- * @author 
- * PUT_YOUR_NAME_HERE
+ * @author
+ * Rob Budak, Alex Harris, Caleb Schlundt
  */
 
-/** namespace. */
+/* eslint-disable no-var */
 var rhit = rhit || {};
+/* eslint-enable no-var */
 
 /** globals */
 rhit.PageManagerSingleton = "";
+
+// From https://stackoverflow.com/a/35385518/12693560
+/**
+ * @param {String} html representing a single element
+ * @return {Element}
+ */
+function htmlToElement(html) {
+	const template = document.createElement('template');
+	html = html.trim(); // Never return a text node of whitespace as the result
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
 rhit.PageController = class {
 	constructor() {
@@ -18,27 +31,54 @@ rhit.PageController = class {
 			console.log("Starting Game");
 			document.querySelector("#entryPage").style.display = "none";
 			document.querySelector("#questionsPage").style.display = "block";
-		}
+		};
 
 		document.querySelectorAll(".answer-button").forEach((element) => {
-			console.log("Answer button clicked:", element.dataset.item);
+			element.onclick = (event) => {
+				console.log("Answer button clicked:", element.dataset.item);
+			};
 		});
 	}
 
 	methodName() {
 
 	}
-}
+};
 
 rhit.PageManager = class {
 	constructor() {
 		console.log("Page Manager Built");
+		this.roomKey = "HHTPZ";
+
+		// TODO switch to actual loading instead of fake data
+		this.updateRoomKey();
+		const playerList = document.querySelector("#connectedPlayers");
+		for (let index = 0; index < 10; index++) {
+			const playerCard = this._createPlayerItem(`Player ${index}`);
+			playerList.appendChild(playerCard);
+		}
 	}
 
-	methodName() {
-
+	updateRoomKey(key) {
+		document.querySelector("#roomKey").innerHTML = `Room Key: ${this.roomKey}`;
 	}
-}
+
+	addPlayerToList(name) {
+		document.querySelector("#roomKey").innerHTML = `Room Key: ${key}`;
+	}
+
+	_createPlayerItem(playerName) {
+		// TODO use cards for players maybe?
+		// return htmlToElement(
+		// 	`<div class="card">
+		// 		<div class="card-body">
+		// 			<h5 class="card-title">${player}</h5>
+		// 		</div>
+		// 	</div>`);
+		return htmlToElement(
+			`<div class="player-item"><p>${playerName}</p></div>`);
+	}
+};
 
 /* Main */
 /** function and class syntax examples */
@@ -47,7 +87,6 @@ rhit.main = function () {
 	rhit.PageManagerSingleton = new rhit.PageManager();
 
 	console.log("Ready");
-
 };
 
 rhit.main();
